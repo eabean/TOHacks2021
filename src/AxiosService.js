9b0dab945c471c3fcf8d06ae1a15778418bf70d7
Enid A <edotadot2@gmail.com>
2021-05-08 16:29:57 -0700

    using opencovidAPI works. Error is from passing authentication headers for TravelPerk API

--- src/AxiosService.js
+++ src/AxiosService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const url = 'https://sandbox.travelperk.com/travelsafe/restrictions?destination=ES&destination_type=country_code&origin=DE&origin_type=country_code&date=2020-10-15';
-
+// const url = 'https://sandbox.travelperk.com/invoices/lines?profile_id=&serial_number=&serial_number_contains=&billing_period=&travelperk_bank_account_number&status=&issuing_date_gte&issuing_date_lte&due_date_gte&due_date_lte&expense_date_gte&expense_date_lte&limit=&offset=';
+const url = 'https://api.opencovid.ca/timeseries?stat=cases&loc=prov&date=01-09-2020';
 class AxiosService {
 
     // get Posts
@@ -9,18 +9,12 @@ class AxiosService {
         return new Promise((resolve, reject) => {
 
             try {
-                axios.get(url, {
-                    headers: {
-                      'Authorization': 'ApiKey GnQYic.zc102IrxZn7XZpD991XJJTIkFFUiutin',
-                      'Api-Version': 1
-                    }
-                }).then(res => {
+                axios.get(url).then(res => {
                         const data = res.data;
                         console.log(data);
                         resolve(data);
 
                 }).catch ((err) => {
-                    // console.log(error);
                     reject(err);
                 });
 
@@ -33,3 +27,14 @@ class AxiosService {
 }
 
 export default AxiosService;
+
+// let headers = {
+//                     'Authorization': 'ApiKey GnQYic.zc102IrxZn7XZpD991XJJTIkFFUiutin',
+//                     'Api-Version': "1"
+//             };
+// {
+//     headers: {
+//       'Authorization': 'ApiKey GnQYic.zc102IrxZn7XZpD991XJJTIkFFUiutin',
+//       'Api-Version': "1"
+//     }
+// }

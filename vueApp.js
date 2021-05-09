c8fcd12b81aacd19b2d7291b052edf25136029a7
Angela <tiangela1@gmail.com>
2021-05-08 18:18:03 -0700

    Added functionality to fetch and display data from Lumos API.

--- vueApp.js
+++ vueApp.js
@@ -1,42 +1,28 @@
-// const url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD,EUR";
-const url = "https://sandbox.travelperk.com/travelsafe/restrictions?destination=ES&destination_type=country_code&origin=DE&origin_type=country_code&date=2020-10-15";
+const url = "https://covid-api.thinklumo.com/data?airport=BOS";
 const apiKey = APIKEY;
 
 const vm = new Vue({
     el: '#app',
-    //Mock data for the value of BTC in USD
     data: {
-        // results: {
-        //     "BTC": { "USD": 3759.91, "EUR": 3166.21 },
-        //     "ETH": { "USD": 281.7, "EUR": 236.25 },
-        //     "NEW Currency":{"USD":5.60,"EUR":4.70}
-        // }
         results: [],
         error: ""
     },
     mounted() {
-        // axios.get(url).then(response => {
-        //     this.results = response.data
-        // });
-
         try {
             axios.get(url, {
                 headers: {
-                    "Authorization": `ApiKey ${apiKey}`,
-                    "Api-Version": 1,
-                    "Accept-Language": "en",
-                    // 'Access-Control-Allow-Origin': "*",
+                    "X-Api-Key": `${apiKey}`,
+                    // "Api-Version": 1,
+                    // "Accept-Language": "en",
+                    "Access-Control-Allow-Origin": "*",
                     // 'Access-Control-Allow-Credentials': "true",
-                    // 'Access-Control-Allow-Origin': "*",
+                    // "Access-Control-Allow-Origin": "*",
                     // 'Access-Control-Allow-Credentials': "true",
-                    // 'Access-Control-Allow-Methods': "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-                    // 'Access-Control-Allow-Headers': "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-                    "Access-Control-Allow-Headers": "Authorization"
+                    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
+                    // "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+                    "Access-Control-Allow-Headers": "Content-Type"
                 }
             }).then((res) => {
-                // const data = res.data;
-                // console.log(res);
-                // resolve(data);
                 this.results = res.data
             }).catch(e => {
                 this.error = e;
